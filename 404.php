8dd209cbea9627e0edcdf4e7b31e85123411c2d0
Btldjus <108800613+Btldjus@users.noreply.github.com>
2022-08-26 14:55:28 +0500

    Content

---
+++ 404.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <link  rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
+    <title>PHP веб сайт</title>
+</head>
+<body>
+
+<?php require "blocks/header.php"?>
+
+<div class="container mt-5">
+    <h3 class="mb-5">Ошибка 404</h3>
+
+<?php require "blocks/footer.php"?>
+</body>
+</html>
---
+++ send.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <link  rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
+    <title>Обратная связь</title>
+</head>
+<body>
+<?php require "blocks/header.php"?>
+
+<h2 class="container">Отправлено!</h2>
+
+<form class="container mt-5 " action="index.php" target="_self"><button class="btn2">На главную</button></form>
+
+<?php require "blocks/footer.php"?>
+
+
+</body>
+</html>
